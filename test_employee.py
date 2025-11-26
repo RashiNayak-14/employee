d68ee6d61f938c4ea57a5d0a61a20fab3863a18f
Rashi.Nayak <01FE24BCA218@kletechac.in>
2025-11-26 13:42:51 +0530

    modified

--- test_employee.py
+++ test_employee.py
@@ -1,18 +1,15 @@
-#test employee.py
-import pytest
+import pytest 
 from employee import get_employee_info
 def test_get_employee_info():
     #sample data
-    name = "Alice Smith"
-    emp_id = "E202"
+    name="Alice Smith"
+    emp_id="E202"
     department = "HR"
     salary = 60000
-    #expected result
     expected_output = (
-        "Employee Name: Alice Smith\n"
-        "Employee ID: E202\n"
-        "Department: HR\n"
-        "Salary: 60000"
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary:.2f}"
     )
- #Assertion
     assert get_employee_info(name, emp_id, department, salary) == expected_output
